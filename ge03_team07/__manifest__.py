e53dcb1351704a449d9240f6956c4013ae334fb1
aala-odoo <aala@odoo.com>
2023-07-25 09:49:32 -0600

    Third module for motorcycle name generation in products tab

---
+++ ge03_team07/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Motorcycle Name Generation",
+    "summary": "Name Generation for motorcycles",
+    "description": """
+    Motorcycle generation
+    ====================
+    This Module is used to generate the name for motorcycles.
+    """,
+    "version": "0.1",
+    "category": "Kawiil/Admin",
+    "license": "OPL-1",
+    "depends": ["stock","motorcycle_registry"],
+    "data": [],
+    "demo": [],
+    "author": "team7",
+    "website": "www.odoo.com",
+    "application": True,
+}
---
+++ ge03_team07/models/motorcycle_name_generation.py
@@ -0,0 +1,10 @@
+from odoo import models, fields, api
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    name = fields.Char(compute="_compute_name",store=True)
+    @api.depends('make','model','year','detailed_type')
+    def _compute_name(self):
+        for record in self.filtered_domain([('detailed_type','=','motorcycle')]):
+            record.name= str(record.year) + str(record.make) + str(record.model)
