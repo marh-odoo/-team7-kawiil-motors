fb031cb03c4a053d3f7ed84146de9ba87088cc64
aala-odoo <aala@odoo.com>
2023-07-26 12:03:28 -0600

    [FIX] ge03_team07: allow this module to autogenerate moto names.
    We couldn't create a new product of any type and we had the auto generated name 0FAFA when the fields make, model and year were not filled.

--- ge03_team07/models/motorcycle_name_generation.py
+++ ge03_team07/models/motorcycle_name_generation.py
@@ -3,8 +3,12 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    name = fields.Char(compute="_compute_name",store=True)
+    name = fields.Char(compute="_compute_name",store=True,readonly=False)
+
     @api.depends('make','model','year','detailed_type')
     def _compute_name(self):
-        for record in self.filtered_domain([('detailed_type','=','motorcycle')]):
-            record.name= str(record.year)[:2] + str(record.make)[:2] + str(record.model)[:2]
+        for record in self:
+            if record.detailed_type == 'motorcycle' and all((record.year, record.make, record.model)):
+                record.name = str(record.year)[0:2] + str(record.make)[0:2] + str(record.model)[0:2]
+            else:
+                record.name =""
