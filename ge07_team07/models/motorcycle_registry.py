71e4cfadc6f73a6d21bf01e297d3d2630fc78b5d
aala-odoo <aala@odoo.com>
2023-07-26 16:39:54 -0600

    [ADD] ge07team07 : add more code to the module.
    We wanted to force the one2one relationship with this change.

--- ge07_team07/models/motorcycle_registry.py
+++ ge07_team07/models/motorcycle_registry.py
@@ -1,11 +1,15 @@
 from odoo import api, fields, models
-from odoo.exceptions import UserError, ValidationError
+from odoo.exceptions import ValidationError
 
 class MotorcycleRegistry(models.Model):
-    _inherit = ['motorcycle.registry']
+    _inherit = 'motorcycle.registry'
     lot_ids = fields.One2many('stock.lot','motorcycle_registry_id')
 
     @api.constrains('lot_ids')
     def _validate_one2one_lot_ids(self):
-        number_lot_ids = self.search()
+        for registry in self:
+            if len(registry.lot_ids) > 1:
+                raise ValidationError("It is not possible to add more that one stock lot")
+
+
     
--- ge07_team07/models/stock_lot.py
+++ ge07_team07/models/stock_lot.py
@@ -2,5 +2,5 @@
 from odoo import api, fields, models
 
 class StockLot(models.Model):
-    _inherit = ['stock.lot']
+    _inherit = 'stock.lot'
     motorcycle_registry_id = fields.Many2one('motorcycle.registry')
