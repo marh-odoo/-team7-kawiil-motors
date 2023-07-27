e5ba49416df0b180f631f3d9e364321131b8de5f
aala-odoo <aala@odoo.com>
2023-07-27 14:57:19 -0600

    [IMP] ge03_team07: make the adjustment suggested by our mentor.
    We did the changes to follow the convention and to fulfill the requirements.

--- ge03_team07/models/product_template.py
+++ ge03_team07/models/product_template.py
@@ -7,8 +7,8 @@ class ProductTemplate(models.Model):
 
     @api.depends('make','model','year','detailed_type')
     def _compute_name(self):
-        for record in self:
-            if record.detailed_type == 'motorcycle' and all((record.year, record.make, record.model)):
-                record.name = str(record.year)[0:2] + str(record.make)[0:2] + str(record.model)[0:2]
+        for product in self:
+            if product.detailed_type == 'motorcycle' and all((product.year, product.make, product.model)):
+                product.name = str(product.year) + " " + str(product.make) + " " +str(product.model)
             else:
-                record.name =""
+                product.name = ""
