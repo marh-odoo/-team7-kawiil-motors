68e2b35710aa09e4ba62275790e34b30b143f2a1
rsma-odoo <rsma@odoo.com>
2023-07-28 17:44:18 -0600

    [ADD]Creates a new drag-and-drop-able widget for web edit.

---
+++ ge10_team7/__manifest__.py
@@ -0,0 +1,43 @@
+{
+    "name": "ge10_team7",
+    
+    "summary": "Adds a widget to display total motorcycle mileage on a web view.",
+    
+    "description": """
+    Total Mileage Count
+====================
+This Module is used to keep track of the current mileage of each motorcycle on the registry.
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Kawiil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["motorcycle_registry", "website"],
+    
+    "data": [
+        "views/mileage_snippet.xml",
+    ],
+    
+    "demo": [
+        "demo/motorcycle_registry_demo.xml",
+        "demo/product_demo.xml",
+    ],
+
+    "assets":{
+        "web.assets_frontend":[
+            "ge10_team7/static/src/js/odometer.js",
+            "ge10_team7/static/styles/odometer_theme_car.css",
+            #"ge10_team7/static/styles/generic.css",
+        ],
+    },
+    
+    "author": "kawiil-motors",
+    
+    "website": "www.odoo.com",
+    
+    "application": True,
+    
+}
---
+++ ge10_team7/controllers/controllers.py
@@ -0,0 +1,7 @@
+from odoo import http
+
+class SnippetControllers(http.Controller):
+
+    @http.route('/get_mileage_count',type='json',website=True)
+    def get_mileage_count(self):
+        return sum(http.request.env['motorcycle.registry'].search([]).mapped('current_mileage'))#or http.request.env
