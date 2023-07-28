1d08d7b99508c1ec984fdc59b39c6ad04631036d
Mauricio Rubio Haro <marh@odoo.com>
2023-07-28 08:55:05 -0600

    [ADD] link to lot id

--- ge09_team07/models/repair_order.py
+++ ge09_team07/models/repair_order.py
@@ -6,9 +6,15 @@ class Repair(models.Model):
     vin = fields.Char(string='VIN', compute="_generate_registry_vin_", readonly=False, required=True)
     milage = fields.Char(string='Milage')
     registry_id = fields.Many2one('motorcycle.registry',string='Registry Id', store=True, compute="_generate_registry_id_")
-    # product_id = fields.Many2one('sale.order', compute="search_order_products", store=True)
-    partner_id = fields.Many2one(related='registry_id.owner_id', store=True)
+    lot_id = fields.Many2one(related='registry_id.lot_id', string="lots ids numbers", store=True)
+    product_id = fields.Many2one('product.product',compute="_generate_product_id_", store=True)
+    # partner_id = fields.Many2one(related='registry_id.owner_id', store=True)
 
+    @api.depends('lot_id')
+    def _generate_product_id_(self):
+        for product in self:
+            product.product_id = product.lot_id.product_id
+            
 
     @api.depends('vin')
     def _generate_registry_id_(self):
@@ -16,7 +22,7 @@ class Repair(models.Model):
             motorcycle = order.env['motorcycle.registry'].search([('vin', 'ilike', order.vin)])
             if motorcycle:
                 order.registry_id = motorcycle
-        
+
     @api.depends('registry_id')
     def search_order_products(self):
         for sale in self:
