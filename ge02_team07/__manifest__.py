a8381423882fcf470a35c8b9d46979c8a141dbd8
cmgv-odoo <cmgv@odoo.com>
2023-08-01 09:39:07 -0600

    [FIX] ge08_team07: Correcting Merge Conflicts

---
+++ ge02_team07/__manifest__.py
@@ -0,0 +1,33 @@
+{
+    "name": "Motorcycle Registry Map View",
+    
+    "summary": "Able to have map view in motorcycle registry view",
+    
+    "description": """
+        Display all motorcycle registry's since map view
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Kawiil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["motorcycle_registry", "web_map"],
+    
+    "data": [
+        'views/motorcycle_registry_menuitems_inherit.xml',
+        'views/motorcycle_registry_views_inherit.xml'
+    ],
+    
+    "demo": [],
+    
+    "author": "team7",
+    
+    "website": "www.odoo.com",
+    
+    "application": False,
+
+    "auto_install": True,
+    
+}
